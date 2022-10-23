1b3f55e59d69a12a33fc3dd64fff338615b3dc5d
dlbcb <diegol08@ucm.es>
2022-10-23 12:05:58 +0200

    Mapa0.1

---
+++ assets/1_Terrains_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="1_Terrains_32x32" tilewidth="32" tileheight="32" tilecount="2144" columns="32">
+ <image source="tileset/Modern_Exteriors_32x32/ME_Theme_Sorter_32x32/1_Terrains_32x32.png" width="1024" height="2144"/>
+</tileset>
---
+++ assets/Modern_Exteriors_Complete_Tileset_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Modern_Exteriors_Complete_Tileset_32x32" tilewidth="32" tileheight="32" tilecount="29744" columns="176">
+ <image source="tileset/Modern_Exteriors_32x32/Modern_Exteriors_Complete_Tileset_32x32.png" width="5632" height="5408"/>
+</tileset>
